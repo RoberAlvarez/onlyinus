d94a18a244cac3815fc3197ffc54dbcb5ae10a07
rober <robeeuu@gmail.com>
2025-05-29 00:15:16 +0200

    cc

--- onlyinus/settings.py
+++ onlyinus/settings.py
@@ -3,9 +3,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-replace-this-key'
+SECRET_KEY = '5vmq3r!&#4p=a1f2a12-8f3k2k7kmzh&s7r(s%o7n*)7p5smk@!k'
+import os
+#SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-replace-this-key')
+
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', 'onlyinus.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
